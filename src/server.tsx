19676a4fc27423d7153f3b492279636c5cf4cedb
Jan-Willem Westerhof <info@westit.nl>
2025-06-09 10:39:43 +0200

    Fixed iFrame code

--- src/server.tsx
+++ src/server.tsx
@@ -258,12 +258,11 @@ export class CentralAuthClass {
 
     const script = `const iframe = document.createElement("iframe");
 iframe.id = "centralauth-embedded-login";
-iframe.border = "none";
 iframe.scrolling = "no";
 iframe.src = "${srcUrl.toString()}";
 iframe.allow = "publickey-credentials-get *; publickey-credentials-create *";
 iframe.referrerpolicy = "origin";
-iframe.style = "width:420px;outline:none";
+iframe.style = "width:420px;outline:none;border:none";
 document.getElementById("centralauth-login-form").innerHTML = iframe.outerHTML;
 window.addEventListener("message", ({data}) => document.getElementById("centralauth-embedded-login").style.height = data + "px");`;
 
